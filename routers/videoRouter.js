7f38e154fa968f2d7bd53625a41ef11b294c158d
sookim <louise.blackolive@gmail.com>
2019-06-05 18:03:50 +0900

    lecture 3.9

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,18 +4,27 @@ import {
   getUpload,
   postUpload,
   videoDetail,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   deleteVideo
 } from "../controllers/videoControllers";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload Video
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// View Detail
+
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
